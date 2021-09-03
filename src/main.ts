7c932d7371cd6aab141776523a21c2e7bdd34b0f
Samuel Ballan <sgb4622@gmail.com>
2021-09-03 12:21:38 -0400

    test prod

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `
   <h1>Hello Vite!</h1>
+  <p>We are up and running</p>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
